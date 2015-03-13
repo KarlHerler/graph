7273d23135d03da751d0f844dc14d1524a480447
Karl Herler <hi@karlherler.com>
2015-03-13 20:50:17 +0200

    Added depth first search by use of the inner_search function

--- src/lib.rs
+++ src/lib.rs
@@ -61,6 +61,24 @@ impl Graph {
     return self.inner_search(start, target, true)
   }
 
+  /// `depth_first_search` implements depth first search from `start` to the
+  /// `target` and returns the path found as a `VecDeque<usize>` of nodes. This
+  /// is an optional type as there might not be a path.
+  ///
+  /// **NOTE** as this is depth first search this search ignores any assigned
+  /// weight to nodes.
+  ///
+  /// ## Arguments
+  /// * `start`  - an `usize` designating the start node, or row in the adjecency matrix
+  /// * `target` - an `usize` designating the target node, or row in the adjecency matrix
+  ///
+  /// ## Returns
+  /// Either the found path between start and target as a `VecDeque` of `usize`:s
+  /// or `None` if there is no path.
+  pub fn depth_first_search(&self, start: usize, target: usize) -> Option<VecDeque<usize>> {
+    return self.inner_search(start, target, false)
+  }
+
   fn inner_search(&self, start: usize, target: usize, bfs: bool) -> Option<VecDeque<usize>>  {
     let mut q: VecDeque<usize> = VecDeque::new();
     let mut discovered: HashSet<usize> = HashSet::new();
@@ -132,11 +150,11 @@ fn breadth_first_search_test() {
   let res = g.breadth_first_search(start, target);
   match res {
     None => {
-      println!("bfs search returned None");
+      println!("Breadth first search returned None");
       assert!(false);
     }
     Some(result) => {
-      println!("Bfs returned something: {:?}", result);
+      println!("Breadth first search returned something: {:?}", result);
       assert_eq!(result[result.len()-1], target);
       assert_eq!(result[0], start);
     }
@@ -160,11 +178,66 @@ fn breadth_first_search_test_no_valid_path() {
   // The expected return value is None
   match res {
     None => {
-      println!("bfs search returned None");
+      println!("Breadth first search returned None");
       assert!(true);
     }
     Some(result) => {
-      println!("Bfs returned something: {:?}", result);
+      println!("Breadth first search returned something: {:?}", result);
+      assert_eq!(result[result.len()-1], target);
+      assert_eq!(result[0], start);
+      assert!(false);
+    }
+  }
+}
+
+#[test]
+fn depth_first_search_test() {
+  let testgraph = vec![vec![Some(0), Some(20), Some(80), Some(50),     None,     None, None],
+                       vec![   None,  Some(0),     None,     None,     None,     None, None],
+                       vec![   None,     None,  Some(0),     None,     None,     None,  Some(50)],
+                       vec![   None,     None,     None,  Some(0), Some(50),     None, None],
+                       vec![   None,     None, Some(20),     None,  Some(0), Some(50),  Some(40)],
+                       vec![   None,     None,     None,     None,     None,  Some(0), None],
+                       vec![   None,     None,     None,     None,     None,     None, Some(0)]];
+  let start: usize = 0;
+  let target: usize = 6;
+  let g = Graph::new(testgraph);
+  let res = g.depth_first_search(start, target);
+  match res {
+    None => {
+      println!("Depth first search returned None");
+      assert!(false);
+    }
+    Some(result) => {
+      println!("Depth first search returned something: {:?}", result);
+      assert_eq!(result[result.len()-1], target);
+      assert_eq!(result[0], start);
+    }
+  }
+}
+
+#[test]
+fn depth_first_search_test_no_valid_path() {
+  let testgraph = vec![vec![Some(0), Some(20), Some(80), Some(50),     None,     None, None],
+                       vec![   None,  Some(0),     None,     None,     None,     None, None],
+                       vec![   None,     None,  Some(0),     None,     None,     None,  Some(50)],
+                       vec![   None,     None,     None,  Some(0), Some(50),     None, None],
+                       vec![   None,     None, Some(20),     None,  Some(0),     None,  Some(40)],
+                       vec![   None,     None,     None,     None,     None,  Some(0), None],
+                       vec![   None,     None,     None,     None,     None,     None, Some(0)]];
+  let start: usize = 0;
+  let target: usize = 5; // There is no valid path between 0 and 5
+  let g = Graph::new(testgraph);
+  let res = g.depth_first_search(start, target);
+
+  // The expected return value is None
+  match res {
+    None => {
+      println!("Depth first search returned None");
+      assert!(true);
+    }
+    Some(result) => {
+      println!("Depth first search returned something: {:?}", result);
       assert_eq!(result[result.len()-1], target);
       assert_eq!(result[0], start);
       assert!(false);
